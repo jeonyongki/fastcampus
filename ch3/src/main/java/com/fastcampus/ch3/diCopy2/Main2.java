de6ea85f7c494611464cd47c62ab902f37d14e6f
jeonyongki <ygygjj@naver.com>
2024-02-28 12:28:12 +0900

    diCopy2

---
+++ ch3/src/main/java/com/fastcampus/ch3/diCopy2/Main2.java
@@ -0,0 +1,50 @@
+package com.fastcampus.ch3.diCopy2;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+class Car{}
+
+class SportsCar extends Car {}
+
+class Truck extends Car {}
+
+class Engine{}
+
+class AppContext{
+  Map map; //객체 저장소
+
+  AppContext(){
+
+    try {
+      Properties p = new Properties();
+      p.load(new FileReader("config.txt"));
+      //Properties에 저장된 내용을 map에 저장.
+      map = new HashMap(p);
+      //클래스 이름으로 객체를 생성해서 다시 map에저장.
+      for(Object key : map.keySet()){
+        Class clazz = Class.forName((String)map.get(key));
+        map.put(key, clazz.newInstance());
+      }
+
+      Class clazz = Class.forName(p.getProperty("car"));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+  Object getBean(String key){
+    return map.get(key);
+  }
+}
+public class Main2 {
+  public static void main(String[] args) throws Exception{
+    AppContext ac = new AppContext();
+    Car car = (Car)ac.getBean("car");
+    Engine engine = (Engine) ac.getBean("engine");
+    System.out.println("car = " + car);
+    System.out.println("engine = " + engine);
+  }
+}
